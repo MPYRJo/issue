7c77479c39c192ccab4b89bc013126d94c91896c
Ppajingae <wkmkyj991112@gmail.com>
2024-06-19 19:48:43 +0900

    feat : DB 및 Swagger 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
-
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
     runtimeOnly("com.h2database:h2")
 
