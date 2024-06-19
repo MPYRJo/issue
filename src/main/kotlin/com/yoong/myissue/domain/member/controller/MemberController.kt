1d9cde77858e1383591a9322b9eb4d4b4aef71f3
Ppajingae <wkmkyj991112@gmail.com>
2024-06-19 20:54:34 +0900

    feat : Member Controller 작성

--- src/main/kotlin/com/yoong/myissue/domain/member/controller/MemberController.kt
+++ src/main/kotlin/com/yoong/myissue/domain/member/controller/MemberController.kt
@@ -1,4 +1,19 @@
 package com.yoong.myissue.domain.member.controller
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/member")
 class MemberController {
+
+    @PostMapping("/signup")
+    fun signup(@RequestBody signupRequest: SignupRequest): ResponseEntity<String> {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.signup(signupRequest))
+    }
 }
