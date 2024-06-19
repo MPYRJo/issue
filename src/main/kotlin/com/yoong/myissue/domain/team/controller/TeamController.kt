f7746cf6f98e0b0d10c83eb874f1443ff07a0168
Ppajingae <wkmkyj991112@gmail.com>
2024-06-19 22:10:00 +0900

    feat : Team Controller 작성

---
+++ src/main/kotlin/com/yoong/myissue/domain/team/controller/TeamController.kt
@@ -0,0 +1,51 @@
+package com.yoong.myissue.domain.team.controller
+
+import com.yoong.myissue.infra.dto.UpdateResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/team")
+class TeamController {
+
+    @PostMapping
+    fun createTeam(
+        @RequestBody createTeamRequest: TeamRequest
+    ): ResponseEntity<String> {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(teamService.createTeam(createTeamRequest))
+    }
+
+    @GetMapping("/{teamId}")
+    fun getTeamById(
+        @PathVariable teamId: Long,
+    ): ResponseEntity<TeamResponse> {
+
+        return ResponseEntity.status(HttpStatus.OK).body(teamService.getTeamById(teamId))
+    }
+
+    @GetMapping
+    fun getTeamList(): ResponseEntity<List<TeamResponse>>{
+
+        return ResponseEntity.status(HttpStatus.OK).body(teamService.getTeamList())
+    }
+
+    @PutMapping("/{teamId}")
+    fun updateTeam(
+        @PathVariable teamId: Long,
+        @RequestBody updateTeamRequest: TeamRequest
+    ):ResponseEntity<UpdateResponse>{
+
+        return ResponseEntity.status(HttpStatus.OK).body(teamService.updateTeam(teamId, updateTeamRequest))
+    }
+
+
+    @DeleteMapping("/{teamId}")
+    fun deleteTeam(
+        @PathVariable teamId: Long,
+    ): ResponseEntity<String>{
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(teamService.deleteTeam(teamId))
+    }
+}
