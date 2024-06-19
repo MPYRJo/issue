535ae37b5516e2f28088c3fb60cb0c0ff0381e23
Ppajingae <wkmkyj991112@gmail.com>
2024-06-19 20:51:42 +0900

    feat : Comment entity, repository 작성

---
+++ src/main/kotlin/com/yoong/myissue/domain/comment/entity/Comment.kt
@@ -0,0 +1,39 @@
+package com.yoong.myissue.domain.comment.entity
+
+import com.yoong.myissue.domain.comment.enum.CheckingStatus
+import com.yoong.myissue.domain.issue.entity.Issue
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "comment")
+class Comment(
+
+    @Column(unique = false, nullable = false)
+    val content: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = false)
+    val checkingStatus: CheckingStatus,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    val member: Member,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "issue_id")
+    val issue: Issue,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id : Long? = null
+
+    @Column(name="created_at", nullable = false, unique = false)
+    @CreationTimestamp
+    val createdAt: LocalDateTime = LocalDateTime.now()
+
+    @Column(name="deleted_at", nullable = false, unique = false)
+    var deletedAt: LocalDateTime? = null
+}
