738007bf841f8f6176141e27728f7490373e8dad
Ppajingae <wkmkyj991112@gmail.com>
2024-06-20 10:36:01 +0900

    feat : Member Login Service 시나리오 작성

--- src/main/kotlin/com/yoong/myissue/domain/member/service/MemberService.kt
+++ src/main/kotlin/com/yoong/myissue/domain/member/service/MemberService.kt
@@ -1,6 +1,9 @@
 package com.yoong.myissue.domain.member.service
 
+import com.yoong.myissue.domain.member.dto.LoginRequest
+import com.yoong.myissue.domain.member.dto.LoginResponse
 import com.yoong.myissue.domain.member.dto.SignupRequest
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 
@@ -13,6 +16,13 @@ class MemberService {
         //TODO("nickName 이 중복이 된다면 duplicatedModelException")
         //TODO("첫번째 비밀 번호와 2번째 비밀번호가 일치 하지 않으면 duplicatedModelException")
         TODO("회원 가입 완료 시에 회원 가입 완료 문구 보여줌")
+    }
+
+    @Transactional
+    fun login(loginRequest: LoginRequest): LoginResponse {
+        //TODO("email 이 없다면 ModelNotFoundException")
+        //TODO("비밀번호가 일치 하지 않다면 InvalidCredentialException")
+        TODO("로그인 완료 시 이메일과 Access Token 을 리턴")
     }
 
 }
