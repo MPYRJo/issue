3058a37ca2b206df171eefcb516036d4e7f0b5e5
Ppajingae <wkmkyj991112@gmail.com>
2024-06-20 16:25:19 +0900

    feat : TeamResponse, MemberResponse 확장 함수 작성

--- src/main/kotlin/com/yoong/myissue/domain/team/dto/TeamResponse.kt
+++ src/main/kotlin/com/yoong/myissue/domain/team/dto/TeamResponse.kt
@@ -2,10 +2,22 @@ package com.yoong.myissue.domain.team.dto
 
 import com.yoong.myissue.domain.issue.dto.IssueResponse
 import com.yoong.myissue.domain.member.dto.MemberResponse
+import com.yoong.myissue.domain.team.entity.Team
 
 data class TeamResponse(
     val id: Long,
     val name: String,
     val issue: List<IssueResponse>,
     val member: List<MemberResponse>
-)
+){
+    companion object{
+        fun from(team: Team): TeamResponse {
+            return TeamResponse(
+                id = team.id!!,
+                name = team.name,
+                issue = team.issues.map { IssueResponse.from(it) },
+                member = team.members.map { MemberResponse.from(it) }
+            )
+        }
+    }
+}
